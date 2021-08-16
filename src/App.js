9c45f43e51c5d944c00221a1462e0346d1b33773
sophiagavrila <msophiagavrila@gmail.com>
2021-08-16 11:26:34 -0400

    edit svg of team-illustration-2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import "style.css"
 import "tailwindcss/dist/base.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
 import Hero from "components/hero/TwoColumnWithInput" 
+import FAQ from "components/faqs/SingleCol"
+import Card from "components/cards/TwoTrendingPreviewCardsWithImage"
+import Feature from "components/features/TwoColWithSteps"
 
 function App() {
   return (
     <AnimationRevealPage>
       <Hero />
-      
+      <Feature />
+      <Card />
+      <FAQ />
     </AnimationRevealPage>
   );
 }
