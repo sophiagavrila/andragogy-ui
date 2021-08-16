a6b55bb99df476b712de768ddaab56e26c8bc8a6
sophiagavrila <msophiagavrila@gmail.com>
2021-08-16 10:08:34 -0400

    add custom t3 logo design

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from "react"
 import "style.css"
 import "tailwindcss/dist/base.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
-// import Hero from "components/hero/FullWidthWithImage"
 import Hero from "components/hero/TwoColumnWithInput" 
-// import Hero from "components/hero/BackgroundAsImageWithCenteredContent"
 
 function App() {
   return (
--- src/components/hero/TwoColumnWithInput.js
+++ src/components/hero/TwoColumnWithInput.js
@@ -24,7 +24,7 @@ const Actions = styled.div`
     ${tw`sm:pr-48 pl-8 py-4 sm:py-5 rounded-full border-2 w-full font-medium focus:outline-none transition duration-300  focus:border-primary-500 hover:border-gray-500`}
   }
   button {
-    ${tw`w-full sm:absolute right-0 top-0 bottom-0 bg-primary-500 text-gray-100 font-bold mr-2 my-4 sm:my-2 rounded-full py-4 flex items-center justify-center sm:w-40 sm:leading-none focus:outline-none hover:bg-primary-900 transition duration-300`}
+    ${tw`w-full sm:absolute right-0 top-0 bottom-0 bg-orange-600 text-gray-100 font-bold mr-2 my-4 sm:my-2 rounded-full py-4 flex items-center justify-center sm:w-40 sm:leading-none focus:outline-none hover:bg-orange-700 transition duration-300`}
   }
 `;
 
@@ -53,7 +53,7 @@ export default ({ roundedHeaderButton }) => {
         <TwoColumn>
           <LeftColumn>
             <Heading>
-              Beautiful React Templates <span tw="text-primary-500">for you.</span>
+              4-Step Approach To <span tw="text-orange-600">Effective Training.</span>
             </Heading>
             <Paragraph>
               Our templates are easy to setup, understand and customize. Fully modular components with a variety of
@@ -64,7 +64,7 @@ export default ({ roundedHeaderButton }) => {
               <button>Get Started</button>
             </Actions>
             <CustomersLogoStrip>
-              <p>Our TRUSTED Customers</p>
+              <p>Curricula in which this method has been utilized</p>
               <img src={CustomersLogoStripImage} alt="Our Customers" />
             </CustomersLogoStrip>
           </LeftColumn>
